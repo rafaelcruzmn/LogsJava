b0611920cdf262726b4c9381ab75913de4ac3121
Rafael Cruz M. Nunes <66894316+rafaelcruzmn@users.noreply.github.com>
2025-12-11 02:10:23 -0300

    Escolha alterado de String para int

--- LogsJava/src/main/java/pss/LogService.java
+++ LogsJava/src/main/java/pss/LogService.java
@@ -18,19 +18,16 @@ public class LogService {
     
     private static ILog getEscolha() {
         
-    String tipo = LogConfiguracao.getTipoLog();
-
-    if (Escolha == null ||
-        (tipo.equalsIgnoreCase("json") && !(Escolha instanceof LogJson)) ||
-        (tipo.equalsIgnoreCase("csv") && !(Escolha instanceof LogCsv))) {
-
-        Escolha = tipo.equalsIgnoreCase("json")
-                ? new LogJson()
-                : new LogCsv();
-    }
-
-    return Escolha;
+        int tipo = LogConfiguracao.getTipoLog();
+        if (Escolha == null || (tipo == 1 && !(Escolha instanceof LogJson)) || (tipo == 0 && !(Escolha instanceof LogCsv))) {
+    Escolha = (tipo == 1)
+            ? new LogJson()
+            : new LogCsv();
 }
+
+return Escolha;
+}
+
 
     private static String dataHoraAtual() {
         LocalDateTime now = LocalDateTime.now();
