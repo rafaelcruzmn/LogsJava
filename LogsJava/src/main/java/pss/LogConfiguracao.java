a003dca51f74025f73e411ddb16b853e7799e06e
Rafael Cruz M. Nunes <rafaelddt1@hotmail.com>
2025-12-11 02:37:28 -0300

    Erro de tipo arrumado de novo

--- LogsJava/src/main/java/pss/LogConfiguracao.java
+++ LogsJava/src/main/java/pss/LogConfiguracao.java
@@ -14,30 +14,34 @@ import java.util.Properties;
 
 public class LogConfiguracao {
     
-    private static final String LOGCONF = "log_conf.properties";
-    private static final String TIPO = "tipo";
-
-  public static int getTipoLog() {
-    Properties props = new Properties();
+private static final String LOGCONF = "log_conf.properties";
+    private static final int TIPO = 0;
     
-    try (FileInputStream in = new FileInputStream(LOGCONF)) {
-        props.load(in);
-        String valor = props.getProperty(TIPO, "0");
-        return Integer.parseInt(valor);
-    } catch (IOException | NumberFormatException e) {
-        return 0;
-    }
-}
-
-    public static void setTipoLog(String tipo) {
-        
+    public static int getTipoLog() {
         Properties props = new Properties();
-        props.setProperty(TIPO, tipo);
+        
+        try (FileInputStream in = new FileInputStream(LOGCONF)) {
+            props.load(in);
 
+            String valorStr = props.getProperty(String.valueOf(TIPO), "0");
+            return Integer.parseInt(valorStr);
+
+        } catch (IOException | NumberFormatException e) {
+            return 0;
+        }
+    }
+    public static void setTipoLog(int tipo) {
+        Properties props = new Properties();
+        try (FileInputStream in = new FileInputStream(LOGCONF)) {
+            props.load(in);
+        } catch (IOException e) {
+        }
+        
+        props.setProperty(String.valueOf(TIPO), String.valueOf(tipo));
         try (FileOutputStream out = new FileOutputStream(LOGCONF)) {
             props.store(out, "Configuração do tipo de log");
         } catch (IOException e) {
-            throw new RuntimeException("Falha ao registrar log no arquivo CSV.", e);        
+            throw new RuntimeException("Falha ao registrar log no arquivo de configuração.", e);
         }
     }
 }
